8e56c97a278143a827752e736cdb4d96ad01da2d
Yashpal Meena <yashpal.meena@screen-magic.com>
2020-04-03 06:49:48 +0530

    [Yashpal] Added new file.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
 console.log('Hurray! I am called.');
+
+var btnCallback = function (t, opts) {
+  return t.popup({
+    title: 'List Stats',
+    url: '../html/list-stats.html',
+    args: { myArgs: 'You can access these with t.arg()' },
+    height: 278 // initial height, can be changed later
+  });
+};
+
 window.TrelloPowerUp.initialize({
     'list-actions': function (t) {
       return t.list('name', 'id')
@@ -6,13 +16,8 @@ window.TrelloPowerUp.initialize({
         console.log(JSON.stringify(list, null, 2));
         return [{
           text: "Get List Stats",
-          callback: function (t) {
-            // Trello will call this if the user clicks on this action
-            // we could for example open a new popover...
-            t.popup({
-            });
-          }
+          callback: btnCallback
         }];
       });
     }
-  });
+});
