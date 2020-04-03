f29b6d6bd530b8526d1c8eb02a8d177bc0f29099
Yashpal Meena <yashpal.meena@screen-magic.com>
2020-04-03 08:03:03 +0530

    [Yashpal] Udated file and added node-fetch dependency.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-console.log('Hurray! I am called.');
-
 var btnCallback = function (t, opts) {
+  console.log(JSON.stringify(t));
+  console.log(JSON.stringify(opts));
   return t.popup({
     title: 'List Stats',
     url: 'list-stats.html',
@@ -13,7 +13,7 @@ window.TrelloPowerUp.initialize({
     'list-actions': function (t) {
       return t.list('name', 'id')
       .then(function (list) {
-        console.log(JSON.stringify(list, null, 2));
+        console.log(JSON.stringify(list));
         return [{
           text: "Get List Stats",
           callback: btnCallback
@@ -22,12 +22,8 @@ window.TrelloPowerUp.initialize({
     }
 });
 
-
-// This code sample uses the 'node-fetch' library:
-// https://www.npmjs.com/package/node-fetch
-import fetch from 'node-fetch';
-listName = 'Close[25]';
-fetch('https://api.trello.com/1/lists/5e79eedda3843225f5d57f9b?name='+listName, {
+const fetch = require('node-fetch');
+fetch('https://api.trello.com/1/lists/5e79eedda3843225f5d57f9b?name=Close[25]', {
   method: 'PUT'
 })
   .then(response => {
