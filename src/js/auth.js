368b0afa425abd6a5dd3891e76a3ec431d516b31
Yashpal Meena <yashpal.meena@screen-magic.com>
2020-04-04 11:21:00 +0530

    [Yashpal] Few minor changes.

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,7 +1,7 @@
 var gotError = function(data) {
     console.log('Failed: ' + JSON.stringify(data));
-}
-
+};
+var allListNames = {};
 var authenticationSuccess = function () {
     console.log('Successful authentication');
     window.Trello.get('boards/5e79ee85907182329109e3d5/lists', function(data) {
@@ -10,12 +10,14 @@ var authenticationSuccess = function () {
             data.forEach(list => {
                 list_id = list['id'];
                 list_name = list['name'];
+                allListNames[list_id] = list_name
                 console.log(`List id: ${list_id} and name: ${list_name}.`);
-                window.Trello.get(`lists/${list_id}/cards`, function(data, list_id, list_name) {
+                window.Trello.get(`lists/${list_id}/cards`, function(data) {
                     cards_count = data.length;
+                    list_id = data['idList'];
+                    list_name = allListNames[list_id];
                     window.Trello.put(`lists/${list_id}`, {name: `${list_name}[${cards_count}]`});   
                 }, gotError);
-
             });
         }
     },
