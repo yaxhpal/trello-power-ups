07e1acd09d32ad1d29f88e2996221194cf7fe3d6
Yashpal Meena <yashpal.meena@screen-magic.com>
2020-04-04 12:11:48 +0530

    [Yashpal] Logging removed.

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,22 +1,22 @@
 var gotError = function(data) {
-    console.log('Failed: ' + JSON.stringify(data));
+    console.log('Card Count Failed: ' + JSON.stringify(data));
 };
 var allListNames = {};
 var authenticationSuccess = function () {
     console.log('Successful authentication');
     window.Trello.get('boards/5e79ee85907182329109e3d5/lists', function(data) {
         if (data != null && data.length > 0) {
-            console.log(JSON.stringify(data));
+            // console.log(JSON.stringify(data));
             data.forEach(list => {
                 list_id = list['id'];
                 list_name = list['name'];
                 allListNames[list_id] = list_name;
-                console.log(`List id: ${list_id} and name: ${list_name}.`);
+                // console.log(`List id: ${list_id} and name: ${list_name}.`);
                 window.Trello.get(`lists/${list_id}/cards`, function(data) {
                     cards_count = data.length;
                     if (cards_count > 0) {
                         card_list_id = data[0]['idList']
-                        card_list_name = allListNames[card_list_id]
+                        card_list_name = allListNames[card_list_id].replace(/\[[0-9]*\]/, "");
                         window.Trello.put(`lists/${card_list_id}`, {name: `${card_list_name}[${cards_count}]`});
                     }
                 }, gotError);
@@ -34,7 +34,7 @@ window.Trello.authorize({
         write: 'true'
     },
     expiration: 'never',
-    interactive: 'false',
+    // interactive: 'false',
     success: authenticationSuccess,
     error: gotError
 });
